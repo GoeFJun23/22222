230e3ddc2ceda97096b34f19d39f0c9d026fe947
GoeFJun23 <117713961+GoeFJun23@users.noreply.github.com>
2022-11-26 20:14:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -207,7 +207,7 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
                 "color": get_color()
             },
             "love_day": {
-                "value": acquainted_days,
+                "value": love_days,
                 "color": get_color()
             },
             "note_en": {
