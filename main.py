31c548813a77fa8d03a794b60fa114758055b486
GoeFJun23 <117713961+GoeFJun23@users.noreply.github.com>
2024-06-06 10:56:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -310,4 +310,5 @@ if __name__ == "__main__":
     for user in users:
         send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en, max_temp, min_temp, sunrise,
                      sunset, category, pm2p5, proposal, chp)
-    os.system("pause")
+    import time
+time.sleep(5)
